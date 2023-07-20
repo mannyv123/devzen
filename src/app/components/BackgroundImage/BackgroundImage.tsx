74b12cad875eec5a4d0593a6ade5218e03548766
Manjot Virdi <virdi_manjot@hotmail.com>
2023-07-19 21:15:33 -0700

    add background image to component

--- src/app/components/BackgroundImage/BackgroundImage.tsx
+++ src/app/components/BackgroundImage/BackgroundImage.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+"use client";
+
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
 
 const BackgroundImage = () => {
-    return <div>BackgroundImage</div>;
+    const [image, setImage] = useState("");
+
+    const getImage = async () => {
+        const result = await fetch("/api/image", {
+            method: "GET",
+        });
+
+        const data = await result.json();
+        setImage(data);
+    };
+
+    useEffect(() => {
+        getImage();
+    }, []);
+
+    //TODO: add loading animation for background
+
+    return (
+        <div className="relative h-full w-full">
+            <Image src={`${image}`} fill={true} alt="background" className="object-cover object-center" />
+        </div>
+    );
 };
 
 export default BackgroundImage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import BackgroundImage from "./components/BackgroundImage/BackgroundImage";
 export default function Home() {
     return (
         <main>
-            <div>
+            <div className="absolute inset-x-0 inset-y-0">
                 <BackgroundImage />
             </div>
         </main>
