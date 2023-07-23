466f342e8b720cab4abbf9ec35cc00d0b260f4cf
Manjot Virdi <virdi_manjot@hotmail.com>
2023-07-22 18:01:31 -0700

    create component for chatgpt icons; configure layout for icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import BackgroundImage from "./components/BackgroundImage/BackgroundImage";
+import ChatGpt from "./components/ChatGpt/ChatGpt";
 import Clock from "./components/Clock/Clock";
 
 export default function Home() {
@@ -10,8 +11,9 @@ export default function Home() {
             <div className="absolute z-10 h-full w-full grid grid-cols-1 grid-rows-layout">
                 <div></div>
                 <div></div>
-                <div className="flex justify-center">
+                <div className="flex flex-col w-full justify-center items-center gap-2">
                     <Clock />
+                    <ChatGpt />
                 </div>
                 <div></div>
                 <div></div>
