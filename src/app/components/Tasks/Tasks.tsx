4a6db4710330a6c860acba3a5fd0adfcf712a420
Manjot Virdi <virdi_manjot@hotmail.com>
2023-07-30 21:02:40 -0700

    add sample tasks; render sample tasks

--- src/app/components/Tasks/Tasks.tsx
+++ src/app/components/Tasks/Tasks.tsx
@@ -3,6 +3,33 @@
 import React, { useRef, useState } from "react";
 import { MdTaskAlt } from "react-icons/md";
 
+//TODO: figure out why task dialog element doesn't have a smooth transition
+
+interface Task {
+    id: string;
+    task: string;
+    timestamp: number;
+}
+
+//temporary
+const sampleData: Task[] = [
+    {
+        id: "1",
+        task: "this is task number 1",
+        timestamp: 1690774802633,
+    },
+    {
+        id: "2",
+        task: "this is task number 2. this is task number 2. this is task number 2. this is task number 2. this is task number 2",
+        timestamp: 1690774802633,
+    },
+    {
+        id: "3",
+        task: "this is task number 3",
+        timestamp: 1690774802633,
+    },
+];
+
 const Tasks = () => {
     const [expanded, setExpanded] = useState(false);
     const tasksRef = useRef<HTMLDialogElement>(null);
@@ -28,7 +55,15 @@ const Tasks = () => {
                     expanded ? "h-1/2" : "h-0"
                 }`}
             >
-                <div className="h-full w-full bg-white rounded-lg bg-opacity-90"></div>
+                <div className="h-full w-full bg-white rounded-lg bg-opacity-90 p-4">
+                    <ul>
+                        {sampleData.map((task) => (
+                            <li key={task.id} className="border-b-2 last:border-none">
+                                {task.task}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </dialog>
             <div
                 className={`relative w-fit flex justify-center items-center ml-4 mt-3 cursor-pointer text-white border rounded-full pr-2 lg:pr-0 hover:pr-2 hover:opacity-100 group ${
