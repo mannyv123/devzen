ecf42ccc5677ea5750dae7e11902cb3d2c0542a2
Manjot Virdi <virdi_manjot@hotmail.com>
2023-09-28 16:23:27 -0700

    update styling

--- src/components/TaskListFeature/TaskListFeature.tsx
+++ src/components/TaskListFeature/TaskListFeature.tsx
@@ -17,7 +17,7 @@ function TaskListFeature({ tasksRef, expanded }: TaskListFeatureProps) {
    return (
       <dialog
          ref={tasksRef}
-         className={`w-full lg:w-1/4  ml-0 bg-transparent pb-6 pt-16 px-4 focus:outline-none overflow-hidden transition-taskContainer duration-500 ease-in-out ${
+         className={`w-full lg:w-1/3 ml-0 bg-transparent pb-6 pt-16 px-4 focus:outline-none overflow-hidden transition-taskContainer duration-500 ease-in-out ${
             expanded ? "h-full opacity-100" : "h-0 opacity-0"
          }`}
       >
