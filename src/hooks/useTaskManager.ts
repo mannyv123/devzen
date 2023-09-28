85311888fd8645321bcefe7596ba0c6462ddf42c
Manjot Virdi <virdi_manjot@hotmail.com>
2023-09-28 14:48:26 -0700

    update api calls for optimistic updates

--- src/hooks/useTaskManager.ts
+++ src/hooks/useTaskManager.ts
@@ -1,11 +1,11 @@
 import { createTask, deleteTask, getTasks, updateElapsedTime, updateTaskStatus } from "@/utils/api";
 import { Task } from "@/utils/types";
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
-const fetchTasks = async (updateFunction: Dispatch<SetStateAction<Task[]>>) => {
+const fetchTasks = async () => {
    try {
       const result = await getTasks();
-      updateFunction(result);
+      return result;
    } catch (error) {
       console.error("Error fetching tasks: ", error);
    }
@@ -16,14 +16,36 @@ export function useTaskManager() {
 
    //Update tasks data on mount
    useEffect(() => {
-      void fetchTasks(setTasks); //Pass the setTasks function as a callback
+      const getTasksData = async () => {
+         const tasksData = await fetchTasks(); //Pass the setTasks function as a callback
+         setTasks(tasksData);
+      };
+
+      void getTasksData();
    }, []);
 
    //Add new task
    const addTask = async (newTask: string) => {
       try {
+         //optimistic update
+         const newTaskObj: Task = {
+            _id: Math.random().toString(),
+            task: newTask,
+            completed: false,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            elapsedTime: 0,
+         };
+
+         const updatedTasks = [newTaskObj, ...tasks];
+         setTasks(updatedTasks);
+
+         //API request to create task
          await createTask(newTask);
-         await fetchTasks(setTasks);
+
+         //Refresh tasks data
+         const result = await fetchTasks();
+         setTasks(result);
       } catch (error) {
          console.error("Error adding task: ", error);
       }
@@ -32,8 +54,19 @@ export function useTaskManager() {
    //Update task completion status
    const updateTaskCompletion = async (taskId: string) => {
       try {
+         //optimistic update
+         const previousTasks = [...tasks];
+         const updatedTasks = previousTasks.map((task) =>
+            taskId === task._id ? { ...task, completed: !task.completed } : task,
+         );
+         setTasks(updatedTasks);
+
+         //API request to create task
          await updateTaskStatus(taskId);
-         await fetchTasks(setTasks);
+
+         //Refresh tasks data
+         const result = await fetchTasks();
+         setTasks(result);
       } catch (error) {
          console.error("Error updating task: ", error);
       }
@@ -42,8 +75,17 @@ export function useTaskManager() {
    //Delete a task
    const removeTask = async (taskId: string) => {
       try {
+         //optimistic update
+         const previousTasks = [...tasks];
+         const updatedTasks = previousTasks.filter((task) => task._id !== taskId);
+         setTasks(updatedTasks);
+
+         //API request to create task
          await deleteTask(taskId);
-         await fetchTasks(setTasks);
+
+         //Refresh tasks data
+         const result = await fetchTasks();
+         setTasks(result);
       } catch (error) {
          console.error("Error deleting task: ", error);
       }
@@ -53,7 +95,9 @@ export function useTaskManager() {
    const updateTaskElapsedTime = async (taskId: string, elapsedTime: number) => {
       try {
          await updateElapsedTime(taskId, elapsedTime);
-         await fetchTasks(setTasks);
+         //Refresh tasks data
+         const result = await fetchTasks();
+         setTasks(result);
       } catch (error) {
          console.error("Error updating task elapsed time: ", error);
       }
