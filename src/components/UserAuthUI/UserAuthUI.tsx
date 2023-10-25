f6e090254b9fc9c8c1ce1450793cc54cb6034e95
Manjot Virdi <virdi_manjot@hotmail.com>
2023-10-24 21:51:31 -0700

    add prettier tailwindcss plugin

--- src/components/UserAuthUI/UserAuthUI.tsx
+++ src/components/UserAuthUI/UserAuthUI.tsx
@@ -20,9 +20,9 @@ function UserAuthUI({
    handleSignUpModal,
 }: UserAuthUIProps) {
    return (
-      <div className='group/options relative text-white flex flex-col items-center gap-3 mr-4 mt-3 z-10 lg:opacity-40 hover:opacity-100'>
-         <div className='relative w-fit flex items-center border rounded-full pr-4 lg:pr-0 group-hover/options:pr-4'>
-            <div className='relative w-10 h-10 p-2'>
+      <div className='group/options relative z-10 mr-4 mt-3 flex flex-col items-center gap-3 text-white hover:opacity-100 lg:opacity-40'>
+         <div className='relative flex w-fit items-center rounded-full border pr-4 group-hover/options:pr-4 lg:pr-0'>
+            <div className='relative h-10 w-10 p-2'>
                {typeof userImageExists === "string" ? (
                   <Image
                      src={userImageExists}
@@ -36,31 +36,31 @@ function UserAuthUI({
                )}
             </div>
             {sessionData ? (
-               <div className='overflow-hidden transition-width duration-300 text-center flex-1 group-hover/options:w-40 group-hover/options:h-fit w-40 lg:w-0 lg:h-0'>
+               <div className='w-40 flex-1 overflow-hidden text-center transition-width duration-300 group-hover/options:h-fit group-hover/options:w-40 lg:h-0 lg:w-0'>
                   <p className='truncate'>{sessionData.user?.name}</p>
                </div>
             ) : (
-               <div className='flex gap-3 lg:gap-0 group-hover/options:gap-3 p-2 lg:p-0 group-hover/options:p-2 items-center'>
+               <div className='flex items-center gap-3 p-2 group-hover/options:gap-3 group-hover/options:p-2 lg:gap-0 lg:p-0'>
                   <div
                      onClick={handleSignIn}
-                     className='group/login cursor-pointer overflow-hidden transition-width duration-300 text-center flex-1 group-hover/options:w-[4.75rem] group-hover/options:h-fit w-[4.75rem] lg:w-0 lg:h-0 flex flex-col items-center justify-between gap-1'
+                     className='group/login flex w-[4.75rem] flex-1 cursor-pointer flex-col items-center justify-between gap-1 overflow-hidden text-center transition-width duration-300 group-hover/options:h-fit group-hover/options:w-[4.75rem] lg:h-0 lg:w-0'
                   >
-                     <p className='group-hover/login:underline underline-offset-[.375rem]'>Login</p>
-                     <div className='flex justify-center gap-2 w-full'>
+                     <p className='underline-offset-[.375rem] group-hover/login:underline'>Login</p>
+                     <div className='flex w-full justify-center gap-2'>
                         <VscGithubInverted size={"1.3rem"} />
                         <MdOutlineEmail size={"1.3rem"} />
                      </div>
                   </div>
-                  <div className='transition-height duration-500 h-[2.75rem] lg:h-0 group-hover/options:h-[2.75rem]'>
+                  <div className='h-[2.75rem] transition-height duration-500 group-hover/options:h-[2.75rem] lg:h-0'>
                      <svg width='2' height='100%' xmlns='http://www.w3.org/2000/svg'>
                         <line x1='1' y1='0' x2='1' y2='100%' stroke='white' stroke-width='2' />
                      </svg>
                   </div>
                   <div
                      onClick={handleSignUpModal}
-                     className='group/signup whitespace-nowrap cursor-pointer overflow-hidden transition-width duration-300 text-center flex-1 group-hover/options:w-[4.75rem] group-hover/options:h-fit w-[4.75rem] lg:w-0 lg:h-0 flex flex-col items-center justify-between gap-1'
+                     className='group/signup flex w-[4.75rem] flex-1 cursor-pointer flex-col items-center justify-between gap-1 overflow-hidden whitespace-nowrap text-center transition-width duration-300 group-hover/options:h-fit group-hover/options:w-[4.75rem] lg:h-0 lg:w-0'
                   >
-                     <p className='group-hover/signup:underline underline-offset-[.375rem]'>
+                     <p className='underline-offset-[.375rem] group-hover/signup:underline'>
                         Sign Up
                      </p>
                      <MdOutlineEmail size={"1.3rem"} />
@@ -71,7 +71,7 @@ function UserAuthUI({
          {sessionData ? (
             <div
                onClick={handleSignOut}
-               className='cursor-pointer overflow-hidden transition-width duration-300 flex flex-1 justify-center items-center w-[12.625rem] lg:w-0 lg:h-0 group-hover:w-[12.625rem] rounded-full bg-white'
+               className='flex w-[12.625rem] flex-1 cursor-pointer items-center justify-center overflow-hidden rounded-full bg-white transition-width duration-300 group-hover:w-[12.625rem] lg:h-0 lg:w-0'
             >
                <p className='whitespace-nowrap text-black'>Sign Out</p>
             </div>
